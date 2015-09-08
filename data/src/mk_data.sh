9edbae8395f4bb59d329d95b2058eec704687adc
n-nishizawa <n-nishizawa@nekojarashi.com>
2015-09-08 21:31:49 +0900

    Make sample data larger

--- data/src/mk_data.sh
+++ data/src/mk_data.sh
@@ -3,5 +3,6 @@ export OMP_NUM_THREADS=4
 #make clean 
 rm ../*.json 
 set -x
-make && time ./mk_data.exe 256 256 8
-#make && time ./mk_data.exe 512 512 16
+#make && time ./mk_data 256 256 8
+#make && time ./mk_data 512 512 16
+make && time ./mk_data 1024 1024 16
